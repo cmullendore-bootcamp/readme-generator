f8a6e611ea17e0f5161c32cca76a356b49ce41b8
Chris Mullendore <chris@vindage.com>
2022-04-18 22:00:22 -0700

    License retrieval and file population works

--- index.js
+++ index.js
@@ -2,14 +2,44 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
 
-// TODO: Create an array of questions for user input
-// NOTE: The array of questions is in the questions.json file
-// and is read in dynamically. This is a more maintainable model
-// than hard-coding the questions in the javascript.
+function populateLicenses(questions) {
+    fetch("https://api.github.com/licenses")
+        .then(response => {
+            if (!response.ok) {
+                throw "Licenses service unavailable";
+            }
 
+            var json = response.json()
+                .then(licenses => {
+                    questions.forEach(element => {
+                        if (element.name === 'license') {
+                            choices = [];
+                            licenses.forEach(l => {
+                                choices.push(l.name);
+                            });
+                            element.choices = choices;
+                        };
+                    });
 
+                    askQuestions(questions, licenses);
+                });
+        });
 
+}
 
+function askQuestions(questions, licenses) {
+    inquirer.prompt(questions)
+        .then(answers => {
+
+            licenses.forEach(lic => {
+                if (answers.license == lic.name) {
+                    answers.license = lic;
+                }
+            });
+
+            writeToFile("README.md", answers);
+        });
+}
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
@@ -33,7 +63,7 @@ ${data.contributing}
 ${data.testing}
 
 ## License
-
+Licensed under the ${data.license.name} available at ${data.license.url}
     `;
 
     fs.writeFileSync(fileName, content);
@@ -50,10 +80,8 @@ function init() {
 
     var questions = JSON.parse(qText);
 
-    inquirer.prompt(questions)
-        .then(answers => {
-            writeToFile("README.md", answers);
-        })
+    populateLicenses(questions);
+
 }
 
 // Function call to initialize app
